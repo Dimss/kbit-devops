fe605b577ebb1a6484c03bae4806c95dc87d8e93
Dmitry Kartsev <dkartsev@redhat.com>
2019-10-01 22:33:53 +0300

    finish sonar init add sonarqube for ocp

--- sonar-init/run.sh
+++ sonar-init/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/hudson.plugins.sonar.SonarGlobalConfiguration.xml
-wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/org.quality.gates.jenkins.plugin.GlobalConfig.xml
-
+wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/sonar-init/hudson.plugins.sonar.SonarGlobalConfiguration.xml
+wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/sonar-init/org.quality.gates.jenkins.plugin.GlobalConfig.xml
+sed -i "s|__SONAR_UI_URL__|$SONAR_UI_URL|g" hudson.plugins.sonar.SonarGlobalConfiguration.xml
+sed -i "s|__SONAR_UI_URL__|$SONAR_UI_URL|g" org.quality.gates.jenkins.plugin.GlobalConfig.xml
 exec "$@"
