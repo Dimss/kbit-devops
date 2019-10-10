03752d52f1cadc0e9b580c5b105bf8a8c1015ff7
Dmitry Kartsev <dkartsev@redhat.com>
2019-10-10 14:21:36 +0300

    update conf file path

--- jenkins-init/run.sh
+++ jenkins-init/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/sonar-init/hudson.plugins.sonar.SonarGlobalConfiguration.xml
-wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/sonar-init/org.quality.gates.jenkins.plugin.GlobalConfig.xml
-wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/sonar-init/io.fabric8.jenkins.openshiftsync.GlobalPluginConfiguration.xml
-wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/sonar-init/scriptApproval.xml
+wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/jenkins-init/hudson.plugins.sonar.SonarGlobalConfiguration.xml
+wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/jenkins-init/org.quality.gates.jenkins.plugin.GlobalConfig.xml
+wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/jenkins-init/io.fabric8.jenkins.openshiftsync.GlobalPluginConfiguration.xml
+wget https://raw.githubusercontent.com/Dimss/kbit-devops/master/jenkins-init/scriptApproval.xml
 sed -i "s|__SONAR_UI_URL__|$SONAR_UI_URL|g" hudson.plugins.sonar.SonarGlobalConfiguration.xml
 sed -i "s|__SONAR_UI_URL__|$SONAR_UI_URL|g" org.quality.gates.jenkins.plugin.GlobalConfig.xml
 sed -i "s|__BC_SYNC_TARGET_NS__|$BC_SYNC_TARGET_NS|g" io.fabric8.jenkins.openshiftsync.GlobalPluginConfiguration.xml
